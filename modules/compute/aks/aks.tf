5167695b40846b4553a0ea7d6bcc50022e169be2
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-01-03 11:45:01 +0200

    Update aks.tf

--- modules/compute/aks/aks.tf
+++ modules/compute/aks/aks.tf
@@ -361,7 +361,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
       http_proxy     = try(http_proxy_config.value.http_proxy, null)
       https_proxy    = try(http_proxy_config.value.https_proxy, null)
       no_proxy       = try(http_proxy_config.value.no_proxy, null)
-      trusted_ca     = try(base64encode(http_proxy_config.value.trusted_ca ))         
+      trusted_ca     = try(http_proxy_config.value.trusted_ca, null ))         
     }
   }
   node_resource_group                 = azurecaf_name.rg_node.result
