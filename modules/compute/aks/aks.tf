f4c56ff9d159d6aba9773915b4a63d316a9a5744
oholubnychyi <88096614+oholubnychyi@users.noreply.github.com>
2022-02-11 15:44:35 +0200

    Update aks.tf

--- modules/compute/aks/aks.tf
+++ modules/compute/aks/aks.tf
@@ -234,8 +234,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   lifecycle {
     ignore_changes = [
       windows_profile,
-      http_proxy_config,
-      no_proxy
+      http_proxy_config["no_proxy"]
     ]
   }
   tags = merge(local.tags, lookup(var.settings, "tags", {}))
