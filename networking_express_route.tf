34f0d171bf84a24e49a7e067faf5677e851e6154
Hamad Riaz <hriaz@mobizinc.com>
2022-03-11 23:03:41 -0800

    Fixed key for express route peering

--- networking_express_route.tf
+++ networking_express_route.tf
@@ -48,11 +48,11 @@ module "express_route_circuit_peerings" {
 
   resource_group_name = coalesce(
     try(local.combined_objects_express_route_circuits[each.value.express_route.lz_key][each.value.express_route.key].resource_group_name, null),
-    try(local.combined_objects_express_route_circuits[local.client_config.landingzone_key][each.value.express_route_key].resource_group_name, null)
+    try(local.combined_objects_express_route_circuits[local.client_config.landingzone_key][each.value.express_route.key].resource_group_name, null)
   )
   express_route_circuit_name = coalesce(
     try(local.combined_objects_express_route_circuits[each.value.express_route.lz_key][each.value.express_route.key].name, null),
-    try(local.combined_objects_express_route_circuits[local.client_config.landingzone_key][each.value.express_route_key].name, null)
+    try(local.combined_objects_express_route_circuits[local.client_config.landingzone_key][each.value.express_route.key].name, null)
   )
 }
 
