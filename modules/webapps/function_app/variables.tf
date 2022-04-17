804b46385c9cb9121fb7ea7c874881ff232b1e64
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-04-17 13:47:16 +0300

    Update variables.tf

--- modules/webapps/function_app/variables.tf
+++ modules/webapps/function_app/variables.tf
@@ -8,15 +8,14 @@ variable "location" {
 
 variable "resource_group_name" {
   description = "(Required) Resource group of the App Service"
-  default = null
-}
-
-variable "resource_groups" {
-  description = "combined objetcs of the resource groups. Either resource_group_name or resource_groups is required."
-  default     = {}
+  default     = null
 }
 
 variable "app_service_plan_id" {
+}
+variable "resource_groups" {
+  description = "combined objetcs of the resource groups. Either resource_group_name or resource_groups is required."
+  default     = {}
 }
 
 variable "tags" {
