adc4520666ed4fb3730c51bd9252555e2c899424
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-04-17 13:49:25 +0300

    Update function_app.tf

--- function_app.tf
+++ function_app.tf
@@ -1,21 +1,21 @@
-
 module "function_apps" {
   source     = "./modules/webapps/function_app"
   depends_on = [module.networking]
   for_each   = local.webapp.function_apps
 
-  name                 = each.value.name
-  client_config        = local.client_config
-  dynamic_app_settings = try(each.value.dynamic_app_settings, {})
-  combined_objects     = local.dynamic_app_settings_combined_objects
-  resource_groups      = local.combined_objects_resource_groups
+  name                       = each.value.name
+  client_config              = local.client_config
+  dynamic_app_settings       = try(each.value.dynamic_app_settings, {})
+  app_settings               = try(each.value.app_settings, null)
+  combined_objects           = local.dynamic_app_settings_combined_objects
+  resource_groups            = local.combined_objects_resource_groups
   resource_group_name = coalesce(
     try(local.combined_objects_resource_groups[each.value.resource_group.lz_key][each.value.resource_group.key].name, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group.key].name, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].name, null),
     try(each.value.resource_group.name, null)
   )
-  location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  location                   = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   app_service_plan_id        = try(each.value.lz_key, null) == null ? local.combined_objects_app_service_plans[local.client_config.landingzone_key][each.value.app_service_plan_key].id : local.combined_objects_app_service_plans[each.value.lz_key][each.value.app_service_plan_key].id
   settings                   = each.value.settings
   application_insight        = try(each.value.application_insight_key, null) == null ? null : module.azurerm_application_insights[each.value.application_insight_key]
@@ -29,8 +29,9 @@ module "function_apps" {
   #                 null
   #                 )
   global_settings = local.global_settings
-  base_tags       = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
-  tags            = try(each.value.tags, null)
+  base_tags       = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
+
+  tags = try(each.value.tags, null)
 
   remote_objects = {
     subnets = try(local.combined_objects_networking[try(each.value.settings.lz_key, local.client_config.landingzone_key)][each.value.settings.vnet_key].subnets, null)
@@ -38,6 +39,7 @@ module "function_apps" {
     private_dns        = local.combined_objects_private_dns
     vnets              = local.combined_objects_networking
     private_endpoints  = try(each.value.private_endpoints, {})
+    resource_groups    = try(each.value.private_endpoints, {}) == {} ? null : local.resource_groups
   }
 }
 
