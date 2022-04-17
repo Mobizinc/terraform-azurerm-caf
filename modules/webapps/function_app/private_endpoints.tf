e5f0b062080677204fa4365fea838b4c9023f194
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-04-17 14:11:47 +0300

    Update private_endpoints.tf

--- modules/webapps/function_app/private_endpoints.tf
+++ modules/webapps/function_app/private_endpoints.tf
@@ -1,3 +1,4 @@
+/*
 module "private_endpoint" {
   source   = "../../networking/private_endpoint"
   for_each = var.remote_objects.private_endpoints
@@ -13,3 +14,4 @@ module "private_endpoint" {
   private_dns     = var.remote_objects.private_dns
   client_config   = var.client_config
 }
+*/
