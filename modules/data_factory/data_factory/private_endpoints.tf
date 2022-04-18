10a09ffb10109d4f924173a5b04f60e4fdd746e0
onlyillusion <onlyilussion@gmail.com>
2022-04-18 12:50:49 +0300

    Fix pe in data_factory module

--- modules/data_factory/data_factory/private_endpoints.tf
+++ modules/data_factory/data_factory/private_endpoints.tf
@@ -4,6 +4,7 @@ module "private_endpoint" {
 
   resource_id     = azurerm_data_factory.df.id
   name            = each.value.name
+  location        = var.location
   resource_groups = var.resource_groups
   subnet_id       = var.remote_objects.vnets[try(var.client_config.landingzone_key, each.value.lz_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   settings        = each.value
