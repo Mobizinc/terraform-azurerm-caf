c172bba9bffa778a3a9140429a3db61eca3ffd95
Hamad Riaz <hriaz@mobizinc.com>
2022-05-11 00:44:07 -0700

    Update volume.tf

--- modules/netapp/volume/volume.tf
+++ modules/netapp/volume/volume.tf
@@ -1,16 +1,16 @@
- resource "azurecaf_name" "volume" {
-   name          = var.settings.name
-   prefixes      = var.global_settings.prefixes
-   resource_type = "azurerm_netapp_volume"
-   random_length = var.global_settings.random_length
-   clean_input   = true
-   passthrough   = var.global_settings.passthrough
-   use_slug      = var.global_settings.use_slug
- }
+# resource "azurecaf_name" "volume" {
+#   name          = var.settings.name
+#   prefixes      = var.global_settings.prefixes
+#   resource_type = "azurerm_netapp_volume"
+#   random_length = var.global_settings.random_length
+#   clean_input   = true
+#   passthrough   = var.global_settings.passthrough
+#   use_slug      = var.global_settings.use_slug
+# }
 
 resource "azurerm_netapp_volume" "volume" {
   # A volume name must be unique within each capacity pool. It must be at least three characters long. You can use any alphanumeric characters.
-  name                = azurecaf_name.volume.result
+  name                = var.settings.name
   resource_group_name = var.resource_group_name
   location            = var.location
   account_name        = var.account_name
