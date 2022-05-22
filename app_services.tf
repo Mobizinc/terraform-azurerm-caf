9e4bed88a73998ed524ae0f9482ded07f616761b
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-05-22 14:08:07 +0300

    Update app_services.tf

--- app_services.tf
+++ app_services.tf
@@ -30,7 +30,7 @@ module "app_services" {
     private_dns        = local.combined_objects_private_dns
     vnets              = local.combined_objects_networking
     private_endpoints  = try(each.value.private_endpoints, {})
-    }  
+  }  
 }
 
 output "app_services" {
