ce72c7308fe9b019f6f42fa11bc355a57fd94681
Hamad Riaz <hriaz@mobizinc.com>
2022-05-27 16:37:35 -0400

    Updated

--- modules/netapp/pool/pool.tf
+++ modules/netapp/pool/pool.tf
@@ -1,16 +1,16 @@
-# resource "azurecaf_name" "pool" {
-#   name          = var.settings.name
-#   prefixes      = var.global_settings.prefixes
-#   resource_type = "azurerm_netapp_pool"
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
+resource "azurecaf_name" "pool" {
+  name          = var.settings.name
+  prefixes      = var.global_settings.prefixes
+  resource_type = "azurerm_netapp_pool"
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_netapp_pool" "pool" {
   # The capacity pool name must be unique for each NetApp account
-  name                = var.settings.name
+  name                = azurecaf_name.pool.result
   account_name        = var.account_name
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -33,7 +33,7 @@ module "volumes" {
   account_name        = var.account_name
   pool_name           = azurerm_netapp_pool.pool.name
   service_level       = azurerm_netapp_pool.pool.service_level
-  subnet_id           = try(var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
+  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   export_policy_rule  = try(each.value.export_policy_rule, {})
   tags                = merge(var.base_tags, try(each.value.tags, {}))
   global_settings     = var.global_settings
