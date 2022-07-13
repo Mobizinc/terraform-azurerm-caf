629f72149d05bc8c71bd427649e7eba9be02366b
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-07-13 20:53:38 +0300

    Update diagnostics.tf

--- modules/databases/postgresql_flexible_server/diagnostics.tf
+++ modules/databases/postgresql_flexible_server/diagnostics.tf
@@ -11,8 +11,8 @@
 
 module "diagnostics" {
    source = "../../diagnostics"
-  # count  = lookup(var.settings, "diagnostic_profiles", null) == null ? 0 : 1
-   count  = try(var.settings.diagnostic_profiles, null) == null ? 0 : 1
+   count  = lookup(var.settings, "diagnostic_profiles", null) == null ? 0 : 1
+   #count  = try(var.settings.diagnostic_profiles, null) == null ? 0 : 1
 
    resource_id       = azurerm_postgresql_flexible_server.postgresql.id
    resource_location = azurerm_postgresql_flexible_server.postgresql.location
