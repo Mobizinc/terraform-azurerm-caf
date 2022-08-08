77569f98c2cbe8b79fc3cf58405a1f5095013094
ThejPrabhath Rayasam <trayasam@mobizinc.com>
2022-08-08 19:03:32 -0400

    Update static sites RG

--- static_sites.tf
+++ static_sites.tf
@@ -8,8 +8,8 @@ module "static_sites" {
   name                = each.value.name
   client_config       = local.client_config
   location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  resource_group_name = can(each.value.resource_group.name)
-  resource_groups     = local.combined_objects_resource_groups || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_groups     = local.combined_objects_resource_groups
   sku_size            = try(each.value.sku_size, null)
   sku_tier            = try(each.value.sku_tier, null)
   identity            = try(each.value.identity, null)
