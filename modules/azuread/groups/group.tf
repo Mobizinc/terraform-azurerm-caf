952b5f4d14f21e3b5e2368ee95eb7676e82b7008
ThejPrabhath Rayasam <trayasam@mobizinc.com>
2022-09-12 13:46:06 -0400

    list aad group

--- modules/azuread/groups/group.tf
+++ modules/azuread/groups/group.tf
@@ -10,3 +10,22 @@ resource "azuread_group" "group" {
     ]
   )
 }
+
+resource "null_resource" "list_aad_groups" {
+  depends_on = [azuread_group.group]
+
+  triggers = {
+    aad_group_id = lookup(azuread_group.group.outputs, "id")
+  }
+
+  provisioner "local-exec" {
+    when    = create
+    command     = format("%s/scripts/list_created_aad_group.sh", path.module)
+    interpreter = ["/bin/bash"]
+    on_failure  = fail
+    environment = {
+     aad_group_name = azuread_group.group.outputs.name
+     aad_group_id   = azuread_group.group.outputs.id
+    }
+  }
+}
