fc4647a0d658987dd5c5ae20749e3160205cad20
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-09-24 11:05:29 +0300

    Update managed_instance.tf

--- modules/databases/mssql_mi/managed_instance.tf
+++ modules/databases/mssql_mi/managed_instance.tf
@@ -71,21 +71,21 @@ resource "azurerm_mssql_managed_database" "mssqlmi" {
   managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
 }
 
-module "private_endpoint" {
-  source   = "../../networking/private_endpoint"
-  for_each = var.remote_objects.private_endpoints
+# module "private_endpoint" {
+#   source   = "../../networking/private_endpoint"
+#   for_each = var.remote_objects.private_endpoints
 
-  resource_id         = azurerm_mssql_managed_instance.mssqlmi.id
-  location            = var.location
-  name                = each.value.name
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
-  settings            = each.value
-  global_settings     = var.global_settings
-  base_tags           = var.base_tags
-  private_dns         = var.remote_objects.private_dns
-  client_config       = var.client_config
-}
+#   resource_id         = azurerm_mssql_managed_instance.mssqlmi.id
+#   location            = var.location
+#   name                = each.value.name
+#   resource_group_name = var.resource_group_name
+#   subnet_id           = var.subnet_id
+#   settings            = each.value
+#   global_settings     = var.global_settings
+#   base_tags           = var.base_tags
+#   private_dns         = var.remote_objects.private_dns
+#   client_config       = var.client_config
+# }
 
 resource "azurerm_mssql_managed_instance_active_directory_administrator" "example" {
   depends_on = [azurerm_mssql_managed_instance.mssqlmi]
