18d4dc634292a026c60114e6dcc4439f245b68b4
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-09-29 19:51:56 +0300

    Update managed_instance.tf

--- modules/databases/mssql_mi/managed_instance.tf
+++ modules/databases/mssql_mi/managed_instance.tf
@@ -64,19 +64,19 @@ resource "azurerm_key_vault_secret" "mssql_managed_instance_administrator_passwo
   }
 }
 
-resource "azurerm_mssql_managed_database" "mssqlmi" {
-  depends_on = [azurerm_mssql_managed_instance.mssqlmi]
-  for_each   = try(var.settings.databases, {})
-  name                = each.value.name
-  managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
-}
+# resource "azurerm_mssql_managed_database" "mssqlmi" {
+#   depends_on = [azurerm_mssql_managed_instance.mssqlmi]
+#   for_each   = try(var.settings.databases, {})
+#   name                = each.value.name
+#   managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
+# }
 
-resource "azurerm_mssql_managed_instance_active_directory_administrator" "mssqlmi_active_directory_administrator" {
-  depends_on = [azurerm_mssql_managed_instance.mssqlmi]
-  for_each =  try(var.active_directory_administrator, {})
+# resource "azurerm_mssql_managed_instance_active_directory_administrator" "mssqlmi_active_directory_administrator" {
+#   depends_on = [azurerm_mssql_managed_instance.mssqlmi]
+#   for_each =  try(var.active_directory_administrator, {})
   
-  managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
-  login_username = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].name, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].name)
-  object_id      = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].id, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].id)
-  tenant_id      = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].tenant_id, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].tenant_id)
-}
+#   managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
+#   login_username = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].name, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].name)
+#   object_id      = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].id, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].id)
+#   tenant_id      = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].tenant_id, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].tenant_id)
+# }
