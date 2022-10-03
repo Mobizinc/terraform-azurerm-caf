60c45e9c47925f6b5ea0d4ec24f9cd7bb3237fd9
Thej Rayasam <trayasam@mobizinc.com>
2022-10-03 19:05:18 -0400

    Update subscriptions.tf

--- modules/subscriptions/subscriptions.tf
+++ modules/subscriptions/subscriptions.tf
@@ -47,23 +47,3 @@ resource "null_resource" "refresh_access_token" {
   }
 
 }
-
-resource "null_resource" "list_sub" {
-  depends_on = [azurerm_subscription.sub]
-  
-  triggers = {
-    subscription_id = azurerm_subscription.sub.0.subscription_id
-  }
-
-  provisioner "local-exec" {
-    when    = create
-    command     = format("%s/scripts/list_created_sub.sh", path.module)
-    interpreter = ["/bin/bash"]
-    on_failure  = fail
-    environment = {
-     sub_name = var.settings.name
-     sub_id = azurerm_subscription.sub.0.subscription_id
-  }
-  }
-
-}
