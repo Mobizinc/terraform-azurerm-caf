d72f5778fd9ddffb70cbd022daad8c1bd7b730a5
Thej Rayasam <trayasam@mobizinc.com>
2022-10-03 16:24:49 -0400

    Update subscriptions.tf

--- subscriptions.tf
+++ subscriptions.tf
@@ -5,10 +5,11 @@ module "subscriptions" {
   for_each = var.subscriptions
 
   global_settings  = local.global_settings
-  local_combined_resources = {
-    # Add combined objects that need to be included in the filter
-    subscriptions         = local.combined_objects_subscriptions,
-  }
+#   local_combined_resources = {
+#     # Add combined objects that need to be included in the filter
+#     subscriptions         = local.combined_objects_subscriptions,
+#   }
+  local_combined_subscriptions = local.combined_objects_subscriptions
   subscription_key = each.key
   settings         = each.value
   client_config    = local.client_config
