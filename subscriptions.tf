9b0911d1ecec48c3bd9e2c3be40dbcdba1fd3f15
Thej Rayasam <trayasam@mobizinc.com>
2022-10-04 08:52:52 -0400

    Update subscriptions.tf

--- subscriptions.tf
+++ subscriptions.tf
@@ -5,7 +5,7 @@ module "subscriptions" {
   for_each = var.subscriptions
 
   global_settings  = local.global_settings
-  resource_id      = each.key.subscription_key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : (each.key.subscription_key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id))
+  resource_id      = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id)
   subscription_key = each.key
   settings         = each.value
   client_config    = local.client_config
