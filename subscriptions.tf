201ec3ff156638aa059b4cba4a518a9480fbcd06
Thej Rayasam <trayasam@mobizinc.com>
2022-10-04 10:03:07 -0400

    Update subscriptions.tf

--- subscriptions.tf
+++ subscriptions.tf
@@ -5,8 +5,8 @@ module "subscriptions" {
   for_each = var.subscriptions
 
   global_settings  = local.global_settings
-  resource_id      = each.value.subscription_id
-  # resource_id      = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id)
+  # resource_id      = each.value.subscription_id
+  resource_id      = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id)
   subscription_key = each.key
   settings         = each.value
   client_config    = local.client_config
@@ -35,14 +35,14 @@ output "subscriptions" {
 
 module "sub_diagnostics" {
   source = "./modules/diagnostics"
-#   for_each = var.subscriptions
-  for_each = {
-    for key, value in local.combined_objects_subscriptions : key => value
-    if try(value.diagnostic_profiles, null) != null
-  }
+  for_each = var.subscriptions
+#   for_each = {
+#     for key, value in local.combined_objects_subscriptions : key => value
+#     if try(value.diagnostic_profiles, null) != null
+#   }
   
-  resource_id       = each.value.resource_id
-  # resource_id       = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : (each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id))
+#   resource_id       = each.value.resource_id
+  resource_id       = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : (each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id))
   resource_location = local.global_settings.regions[local.global_settings.default_region]
   diagnostics       = local.combined_diagnostics
   profiles          = try(each.value.diagnostic_profiles, null)
