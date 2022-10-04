bad18c8d7af1de1437a4ad23707c0ba85882e07b
Thej Rayasam <trayasam@mobizinc.com>
2022-10-04 13:42:11 -0400

    Update subscriptions.tf

--- subscriptions.tf
+++ subscriptions.tf
@@ -42,12 +42,12 @@ module "subscription_diagnostics" {
 #     if try(value.diagnostic_profiles, null) != null
 #   }
   
-  resource_id       = each.value.subscription_id
-#   resource_id       = coalesce(
-#     try(format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)), null),
-#     try(format("/subscriptions/%s", each.value.subscription_id), null),
-#     format("/subscriptions/%s", local.client_config.subscription_id)
-#   )
+#   resource_id       = each.value.subscription_id
+  resource_id       = coalesce(
+    try(format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)), null),
+    try(format("/subscriptions/%s", each.value.subscription_id), null),
+    format("/subscriptions/%s", local.client_config.subscription_id)
+  )
   resource_location = local.global_settings.regions[local.global_settings.default_region]
   diagnostics       = local.combined_diagnostics
   profiles          = try(each.value.diagnostic_profiles, null)
