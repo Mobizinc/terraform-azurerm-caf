81a54c3aa9ae71f6fb6e7ac5466a021b4ed9cce9
Thej Rayasam <trayasam@mobizinc.com>
2022-10-04 16:50:58 -0400

    Update subscriptions.tf

--- subscriptions.tf
+++ subscriptions.tf
@@ -5,9 +5,6 @@ module "subscriptions" {
   for_each = var.subscriptions
 
   global_settings  = local.global_settings
-  # resource_id      = each.value.subscription_id
-  # resource_id      = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : each.key == "other_subscription" ? format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)) : format("/subscriptions/%s", each.value.subscription_id)
-  resource_id      = each.key == "logged_in_subscription" ? format("/subscriptions/%s", local.client_config.subscription_id) : format("/subscriptions/%s", each.value.subscription_id)
   subscription_key = each.key
   settings         = each.value
   client_config    = local.client_config
@@ -37,12 +34,7 @@ output "subscriptions" {
 module "subscription_diagnostics" {
   source = "./modules/diagnostics"
   for_each = local.compute.subscription_diagnostics
-#   for_each = {
-#     for key, value in local.combined_objects_subscriptions : key => value
-#     if try(value.diagnostic_profiles, null) != null
-#   }
-  
-#   resource_id       = each.value.subscription_id
+
   resource_id       = coalesce(
     try(format("/subscriptions/%s", try(local.combined_objects_subscriptions[try(each.value.subscription.lz_key, local.client_config.landingzone_key)][each.value.subscription.key].subscription_id, null)), null),
     try(format("/subscriptions/%s", each.value.subscription_id), null),
