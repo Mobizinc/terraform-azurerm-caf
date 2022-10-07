2497484e1f81007618778537379554a3c73808e1
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-10-07 07:45:50 +0000

    Update active_directory_administrator.tf

--- modules/databases/mssql_mi/active_directory_administrator.tf
+++ modules/databases/mssql_mi/active_directory_administrator.tf
@@ -1,9 +1,9 @@
 resource "azurerm_mssql_managed_instance_active_directory_administrator" "mssqlmi_active_directory_administrator" {
   depends_on = [azurerm_mssql_managed_instance.mssqlmi]
   for_each =  try(var.active_directory_administrator, {})
-#   lifecycle {
-#     ignore_changes = [managed_instance_id]
-#   }
+  lifecycle {
+    ignore_changes = [managed_instance_id]
+  }
   managed_instance_id = try(each.value.managed_instance_id, azurerm_mssql_managed_instance.mssqlmi.id)
   login_username      = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].name, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].name)
   object_id           = try(var.azuread_groups[var.client_config.landingzone_key][each.value.azuread_group_key].id, var.azuread_groups[each.value.lz_key][each.value.azuread_group_key].id)
