c4be86afca50323561a4bda3eb5a6a3b2f856545
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-10-12 06:32:14 +0000

    Update output.tf

--- modules/databases/mssql_mi/output.tf
+++ modules/databases/mssql_mi/output.tf
@@ -14,5 +14,5 @@ output "location" {
 
 output "principal_id" {
   value       = try(azurerm_mssql_managed_instance.mssqlmi.identity[0].principal_id, null)
-  description = "SQL MI Id"
+  description = "SQL MI principal id"
 }
