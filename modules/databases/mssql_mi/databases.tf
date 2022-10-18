cbc07ef406101ea2529b7aeb23b36ef54a01ccbd
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-10-18 09:11:37 +0000

    Update databases.tf

--- modules/databases/mssql_mi/databases.tf
+++ modules/databases/mssql_mi/databases.tf
@@ -4,3 +4,18 @@ resource "azurerm_mssql_managed_database" "mssqlmi" {
   name                = each.value.name
   managed_instance_id = azurerm_mssql_managed_instance.mssqlmi.id
 }
+
+resource "azurerm_resource_group_template_deployment" "backupltr" {
+  for_each            = try(var.settings.databases, {})
+  
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  deployment_mode     = "Incremental"
+  parameters_content  = jsonencode({
+  
+    "serverName"       = { value = azurerm_mssql_managed_instance.mssqlmi.name }
+    "dbName"           = { value = each.value.name }
+    "retentionDays"    = { value = each.value.retentionDays }
+  })
+  template_content     = file(local.arm_filename)
+}
