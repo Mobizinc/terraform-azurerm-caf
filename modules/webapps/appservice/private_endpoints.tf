348a4c0d3a2ca6a3f91aa745d4ce48cf164a6773
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-10-28 09:49:42 +0000

    Update private_endpoints.tf

--- modules/webapps/appservice/private_endpoints.tf
+++ modules/webapps/appservice/private_endpoints.tf
@@ -1,16 +1,16 @@
-module "private_endpoint" {
-  source   = "../../networking/private_endpoint"
-  for_each = var.remote_objects.private_endpoints
-  depends_on = [azurerm_app_service.app_service,azurerm_app_service_slot.slots]
+# module "private_endpoint" {
+#   source   = "../../networking/private_endpoint"
+#   for_each = var.remote_objects.private_endpoints
+#   depends_on = [azurerm_app_service.app_service,azurerm_app_service_slot.slots]
   
-  resource_id         = azurerm_app_service.app_service.id
-  location            = var.location
-  name                = each.value.name
-  resource_group_name = var.resource_group_name
-  subnet_id           = try(var.remote_objects.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.remote_objects.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
-  settings            = each.value
-  global_settings     = var.global_settings
-  base_tags           = var.base_tags
-  private_dns         = var.remote_objects.private_dns
-  client_config       = var.client_config
-}
+#   resource_id         = azurerm_app_service.app_service.id
+#   location            = var.location
+#   name                = each.value.name
+#   resource_group_name = var.resource_group_name
+#   subnet_id           = try(var.remote_objects.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.remote_objects.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
+#   settings            = each.value
+#   global_settings     = var.global_settings
+#   base_tags           = var.base_tags
+#   private_dns         = var.remote_objects.private_dns
+#   client_config       = var.client_config
+# }
