7b3064048e3885090010225de958a196909d2d82
ThejPrabhath Rayasam <trayasam@mobizinc.com>
2022-11-07 21:57:30 -0500

    test

--- modules/logic_app/standard/scripts/api_permission.sh
+++ modules/logic_app/standard/scripts/api_permission.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
 appName="iam-automation-logicapp"
-graphId=$(az ad sp list --query "[?appDisplayName=='Microsoft Graph'].appId | [0]" --all)
+graphId=$(az ad sp list --query "[?appDisplayName=='Microsoft Graph'].appId | [0]" --all --out tsv)
+echo "graphId is $graphId"
 principalId=$(az resource list -n $appName --query [*].identity.principalId --out tsv)
+echo "principalId is $principalId"
 graphResourceId=$(az ad sp list --display-name "Microsoft Graph" --query [0].id --out tsv)
+echo "graphResourceId is $graphResourceId"
 appRoleIds=($(az ad sp show --id $graphId --query "appRoles[?value=='Application.Read.All'].id | [0]") $(az ad sp show --id $graphId --query "appRoles[?value=='Application.ReadWrite.All'].id | [0]") $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.Read.All'].id | [0]") $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.ReadWrite.ApplicationConfiguration'].id | [0]") $(az ad sp show --id $graphId --query "appRoles[?value=='Directory.Read.All'].id | [0]") $(az ad sp show --id $graphId --query "appRoles[?value=='User.Read.All'].id | [0]"))
 
 for appRoleId in "${appRoleIds[@]}";
 do
+  echo "appRoleId is $appRoleId"
 	body=$( jq -n \
             --arg principalId "$principalId" \
             --arg graphResourceId "$graphResourceId" \
