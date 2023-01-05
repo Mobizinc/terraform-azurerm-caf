27ebd2fd6ccfcd2b50bc586d194cdcdf90afef49
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-01-05 20:40:30 +0200

    Update container_groups.tf

--- container_groups.tf
+++ container_groups.tf
@@ -3,16 +3,17 @@ module "container_groups" {
   for_each   = local.compute.container_groups
   depends_on = [module.dynamic_keyvault_secrets]
 
-  base_tags                = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].tags : {}
-  client_config            = local.client_config
-  combined_diagnostics     = local.combined_diagnostics
-  diagnostic_profiles      = try(each.value.diagnostic_profiles, {})
-  global_settings          = local.global_settings
-  location                 = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  resource_group_name      = local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].name
-  settings                 = each.value
-  dynamic_keyvault_secrets = try(local.security.dynamic_keyvault_secrets, {})
-
+  base_tags                  = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].tags : {}
+  client_config              = local.client_config
+  combined_diagnostics       = local.combined_diagnostics
+  diagnostic_profiles        = try(each.value.diagnostic_profiles, {})
+  global_settings            = local.global_settings
+  location                   = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  resource_group_name        = local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].name
+  settings                   = each.value
+  dynamic_keyvault_secrets   = try(local.security.dynamic_keyvault_secrets, {})
+  storage_account_name       = try(data.azurerm_storage_account.storage_accounts[each.key].name, null)
+  storage_account_access_key = try(data.azurerm_storage_account.storage_accounts[each.key].primary_access_key, null)
   combined_resources = {
     keyvaults          = local.combined_objects_keyvaults
     managed_identities = local.combined_objects_managed_identities
@@ -20,6 +21,16 @@ module "container_groups" {
   }
 }
 
+data "azurerm_storage_account" "storage_accounts" {
+  for_each = {
+    for key, value in local.compute.container_groups : key => value
+    if try(value.storage_account_key, null) != null
+  }
+
+  name                = module.storage_accounts[each.value.storage_account_key].name
+  resource_group_name = module.storage_accounts[each.value.storage_account_key].resource_group_name
+}
+    
 module "network_profiles" {
   source   = "./modules/networking/network_profile"
   for_each = local.networking.network_profiles
