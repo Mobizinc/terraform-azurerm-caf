6d38abdfc4aa2661d41dc9ac9d68de5f3123db05
Thej Rayasam <trayasam@mobizinc.com>
2023-02-27 08:27:50 -0500

    Update api_permission.sh

--- modules/logic_app/standard/scripts/api_permission.sh
+++ modules/logic_app/standard/scripts/api_permission.sh
@@ -2,19 +2,22 @@
 
 set -x
 
-appName="iam-automation-logicapp"
-graphId=$(az ad sp list --query "[?appDisplayName=='Microsoft Graph'].appId | [0]" --all --out tsv)
-echo "graphId is $graphId"
-principalId=$(az resource list -n $appName --query [*].identity.principalId --out tsv)
-echo "principalId is $principalId"
-graphResourceId=$(az ad sp list --display-name "Microsoft Graph" --query [0].id --out tsv)
-echo "graphResourceId is $graphResourceId"
-appRoleIds=($(az ad sp show --id $graphId --query "appRoles[?value=='Application.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Application.ReadWrite.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.ReadWrite.ApplicationConfiguration'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Directory.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='User.Read.All'].id | [0]" --out tsv))
-
-for appRoleId in "${appRoleIds[@]}";
+appName=(iam-automation-nonprod-dev iam-automation-nonprod-uat)
+for app in "${appName[@]}"
 do
-  echo "appRoleId is $appRoleId"
-	body='{"principalId":"'"$principalId"'","resourceId":"'"$graphResourceId"'","appRoleId":"'"$appRoleId"'"}'
-	az rest --method post --uri https://graph.microsoft.com/v1.0/servicePrincipals/$principalId/appRoleAssignments --body $body --headers Content-Type=application/json
-  echo "appRoleId: $appRoleId is assigned"
+  graphId=$(az ad sp list --query "[?appDisplayName=='Microsoft Graph'].appId | [0]" --all --out tsv)
+  echo "graphId is $graphId"
+  principalId=$(az resource list -n $app --query [*].identity.principalId --out tsv)
+  echo "principalId is $principalId"
+  graphResourceId=$(az ad sp list --display-name "Microsoft Graph" --query [0].id --out tsv)
+  echo "graphResourceId is $graphResourceId"
+  appRoleIds=($(az ad sp show --id $graphId --query "appRoles[?value=='Application.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Application.ReadWrite.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Policy.ReadWrite.ApplicationConfiguration'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='Directory.Read.All'].id | [0]" --out tsv) $(az ad sp show --id $graphId --query "appRoles[?value=='User.Read.All'].id | [0]" --out tsv))
+
+  for appRoleId in "${appRoleIds[@]}";
+  do
+    echo "appRoleId is $appRoleId"
+	  body='{"principalId":"'"$principalId"'","resourceId":"'"$graphResourceId"'","appRoleId":"'"$appRoleId"'"}'
+	  az rest --method post --uri https://graph.microsoft.com/v1.0/servicePrincipals/$principalId/appRoleAssignments --body $body --headers Content-Type=application/json
+    echo "appRoleId: $appRoleId is assigned"
+  done
 done
