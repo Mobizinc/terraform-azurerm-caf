f711c0b5b0d2bb9fd48033238c9acc60d909ef93
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-08-23 15:15:29 +0300

    Update main.tf

--- modules/databases/mssql_mi/main.tf
+++ modules/databases/mssql_mi/main.tf
@@ -14,6 +14,6 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-  tags         = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
+  tags         = merge(local.module_tag, try(var.settings.tags, null))
 
 }
