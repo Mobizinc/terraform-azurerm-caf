d2adb54435edb23ed0490124e1e0dc61e6420f97
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-08-24 10:15:19 +0300

    Update variables.tf

--- modules/webapps/static_site/variables.tf
+++ modules/webapps/static_site/variables.tf
@@ -55,6 +55,7 @@ variable "global_settings" {
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = bool
+  default = true
 }
 
 variable "diagnostic_profiles" {
