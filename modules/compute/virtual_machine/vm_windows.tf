afa7e513f96adebd0edbe67a385544bd3b55ec6c
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-08-24 17:18:54 +0300

    Update vm_windows.tf

--- modules/compute/virtual_machine/vm_windows.tf
+++ modules/compute/virtual_machine/vm_windows.tf
@@ -204,7 +204,7 @@ resource "random_password" "admin" {
 resource "azurerm_key_vault_secret" "admin_password" {
   for_each = local.os_type == "windows" && try(var.settings.virtual_machine_settings[local.os_type].admin_password_key, null) == null ? var.settings.virtual_machine_settings : {}
 
-  name         = format("%s-admin-password", data.azurecaf_name.windows_computer_name[each.key].result)
+  name         = format("%s-admin-password", azurecaf_name.windows_computer_name[each.key].result)
   value        = random_password.admin[local.os_type].result
   key_vault_id = local.keyvault.id
 
