2894349953d78883bb67b0ebed0c2f7e14e5c57d
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-08-24 11:24:51 +0300

    Update main.tf

--- modules/webapps/static_site/main.tf
+++ modules/webapps/static_site/main.tf
@@ -16,7 +16,7 @@ locals {
   }
   tags = var.base_tags ? merge(
     var.global_settings.tags,
-    try(var.resource_group.tags, null),
+    try(var.resource_groups.tags, null),
     local.module_tag,
     try(var.settings.tags, null)
     ) : merge(
