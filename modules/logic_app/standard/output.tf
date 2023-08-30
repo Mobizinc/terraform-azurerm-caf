b067302ec6c2dff5a6784994e30fe893649ec1e2
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-08-30 18:12:29 +0300

    Update output.tf

--- modules/logic_app/standard/output.tf
+++ modules/logic_app/standard/output.tf
@@ -1,14 +1,14 @@
 output "id" {
-  value       = azurerm_logic_app_standard.logic_app_standard.id
+  value       = azurerm_logic_app_standard.logic_app.id
   description = "The ID of the Logic App Standard Instance"
 }
 
 output "name" {
-  value       = azurerm_logic_app_standard.logic_app_standard.name
+  value       = azurerm_logic_app_standard.logic_app.name
   description = "The name of the Logic App Standard Instance"
 }
 
 output "rbac_id" {
-  value       = try(azurerm_logic_app_standard.logic_app_standard.identity.0.principal_id, null)
+  value       = try(azurerm_logic_app_standard.logic_app.identity.0.principal_id, null)
   description = "The Principal ID of the App Service."
 }
