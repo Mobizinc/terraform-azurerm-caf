b640034fac64c5550b41423955d0d73951c0622b
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-09-08 12:00:29 +0300

    Update variables.tf

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -10,3 +10,5 @@ variable "resource_group_name" {
   description = "(Required) The name of the resource group where to create the resource."
   type        = string
 }
+variable "lock_resource" {
+}
