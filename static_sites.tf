06a919a3b5f6fb77713e63ba372c25895cb671b9
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-09-11 12:26:56 +0300

    Update static_sites.tf

--- static_sites.tf
+++ static_sites.tf
@@ -35,3 +35,37 @@ module "static_sites" {
 output "static_sites" {
   value = module.static_sites
 }
+
+module "static_webapps" {
+  source   = "./modules/webapps/static_webapps"
+  for_each = local.webapp.static_webapps
+
+  name                = each.value.name
+  client_config       = local.client_config
+  location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_groups     = local.combined_objects_resource_groups
+  sku_size            = try(each.value.sku_size, null)
+  sku_tier            = try(each.value.sku_tier, null)
+  identity            = try(each.value.identity, null)
+  global_settings     = local.global_settings
+  settings            = each.value
+  base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
+  diagnostic_profiles = try(each.value.diagnostic_profiles, null)
+  diagnostics         = local.combined_diagnostics
+  tags                = try(each.value.tags, null)
+
+  
+  remote_objects = {
+    subnets            = try(local.combined_objects_networking[try(each.value.settings.lz_key, local.client_config.landingzone_key)][each.value.settings.vnet_key].subnets, null)
+    resource_groups    = try(each.value.private_endpoints, {}) == {} ? null : local.resource_groups
+    private_dns        = local.combined_objects_private_dns
+    vnets              = local.combined_objects_networking
+    private_endpoints  = try(each.value.private_endpoints, {})
+    keyvault_id        = can(each.value.keyvault.key) ? local.combined_objects_keyvaults[try(each.value.keyvault.lz_key, local.client_config.landingzone_key)][each.value.keyvault.key].id : null
+    }
+}
+
+output "static_webapps" {
+  value = module.static_webapps
+}
