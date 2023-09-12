1964a6cd2968e2e1062245bef24f1d2b38cfd8d6
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-09-12 16:23:40 +0300

    Update module.tf

--- modules/webapps/static_webapps/module.tf
+++ modules/webapps/static_webapps/module.tf
@@ -1,5 +1,5 @@
 data "azurerm_key_vault_secret" "static_webapps_token" {
-  count          = var.key_vault_secret != "" && var.remote_objects.keyvault_id != "" ? 1 : 0
+  count          = var.settings.repositoryurl != null && var.settings.branch ? 1 : 0
   name           = var.key_vault_secret
   key_vault_id   = var.remote_objects.keyvault_id
 }
