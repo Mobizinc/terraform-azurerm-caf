ceaa9dcd68a47cfc201cdb7e20ddf49a9d243a0d
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-09-12 14:58:08 +0300

    Update module.tf

--- modules/webapps/static_webapps/module.tf
+++ modules/webapps/static_webapps/module.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault_secret" "static_webapps_token" {
   count          = var.key_vault_secret != "" && var.remote_objects.keyvault_id != "" ? 1 : 0
-  name           = var.settings.keyvault_secret_name
+  name           = var.key_vault_secret
   key_vault_id   = var.remote_objects.keyvault_id
 }
 
@@ -10,25 +10,25 @@ resource "azapi_resource" "static_webapps" {
   location = var.location
   parent_id = var.resource_group_name
   identity {
-    type = var.sku == Standard ? var.identity.type : null
+    type = var.settings.sku == Standard ? var.identity.type : null
     identity_ids = lower(var.identity.type) == "userassigned" ? local.managed_identities : null
   }
   body = jsonencode({
     properties = {
-      branch = try(each.value.branch, null)
+      branch = try(var.settings.branch, null)
       buildProperties = {
-        appLocation = try(each.value.applocation, null)
-        githubActionSecretNameOverride = try(each.value.githubactionsecretname, null)
-        apiLocation = try(each.value.apilocation, null)
+        appLocation = try(var.settings.applocation, null)
+        githubActionSecretNameOverride = try(var.settings.githubactionsecretname, null)
+        apiLocation = try(var.settings.apilocation, null)
 
       }
-      publicNetworkAccess = try(each.value.publicNetworkAccess, "Enabled")
+      publicNetworkAccess = try(var.settings.publicNetworkAccess, "Enabled")
       repositoryToken = try( data.azurerm_key_vault_secret.static_webapps_token[0].value , null)
-      repositoryUrl = try(each.value.repositoryurl, null)
+      repositoryUrl = try(var.settings.repositoryurl, null)
     }
     sku = {
-      name = try(each.value.sku , "Free")
-      tier = try(each.value.sku , "Free")
+      name = try(var.settings.sku , "Free")
+      tier = try(var.settings.sku , "Free")
     }
 
   })
