000e25afbebe005a748a9e1951e8bf26d363e26a
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-09-18 09:58:08 +0300

    Update module.tf

--- modules/webapps/static_webapps/module.tf
+++ modules/webapps/static_webapps/module.tf
@@ -1,5 +1,4 @@
 data "azurerm_key_vault_secret" "static_webapps_token" {
-  count          = var.key_vault_secret!= null ? 1 : 0
   name           = var.key_vault_secret
   key_vault_id   = var.remote_objects.keyvault_id
 }
@@ -18,18 +17,17 @@ resource "azapi_resource" "static_webapps" {
     properties = {
       branch = try(var.settings.branch, null)
       buildProperties = {
-        appLocation = try(var.settings.applocation, null)
-        githubActionSecretNameOverride = try(var.settings.githubactionsecretname, null)
-        apiLocation = try(var.settings.apilocation, null)
+        appLocation = try(var.settings.app_location, null)
+        apiLocation = try(var.settings.api_location, null)
 
       }
-      publicNetworkAccess = try(var.settings.publicNetworkAccess, "Enabled")
-      repositoryToken = try( data.azurerm_key_vault_secret.static_webapps_token[0].value , null)
-      repositoryUrl = try(var.settings.repositoryurl, null)
+      publicNetworkAccess = try(var.settings.public_access, "Enabled")
+      repositoryToken = data.azurerm_key_vault_secret.static_webapps_token.value 
+      repositoryUrl = try(var.settings.repository_url, null)
     }
     sku = {
-      name = try(var.settings.sku , "Free")
-      tier = try(var.settings.sku , "Free")
+      name = try(var.settings.sku , "Standard")
+      tier = try(var.settings.sku , "Standard")
     }
 
   })
@@ -38,4 +36,21 @@ resource "azapi_resource" "static_webapps" {
     "id",
     "location"
   ]
+}
+
+module "private_endpoint" {
+  depends_on = [azapi_resource.static_webapps]
+  source   = "../../networking/private_endpoint"
+  for_each = var.remote_objects.private_endpoints
+
+  resource_id         = azapi_resource.static_webapps.id
+  location            = var.location
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  subnet_id           = try(var.remote_objects.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.remote_objects.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
+  settings            = each.value
+  global_settings     = var.global_settings
+  base_tags           = var.base_tags
+  private_dns         = var.remote_objects.private_dns
+  client_config       = var.client_config
 }
