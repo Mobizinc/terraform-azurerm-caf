9fe4795bb973f315329a3bbf4197c16f27a0ca45
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-10-06 14:24:13 +0300

    Update configuration.tf

--- modules/databases/postgresql_flexible_server/configuration.tf
+++ modules/databases/postgresql_flexible_server/configuration.tf
@@ -1,9 +1,9 @@
-# resource "time_sleep" "server_configuration" {
-#   depends_on = [azurerm_postgresql_flexible_server.postgresql]
+ resource "time_sleep" "server_configuration" {
+   depends_on = [azurerm_postgresql_flexible_server.postgresql]
 
-#   create_duration  = "120s"
-#   destroy_duration = "300s"
-# }
+   create_duration  = "120s"
+   destroy_duration = "300s"
+ }
 
 resource "azurerm_postgresql_flexible_server_configuration" "postgresql" {
   # depends_on = [time_sleep.server_configuration]
