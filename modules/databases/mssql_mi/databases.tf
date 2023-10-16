7556facdc531010ad67752c033bb3ff273c714bd
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-10-16 11:25:04 +0300

    Update databases.tf

--- modules/databases/mssql_mi/databases.tf
+++ modules/databases/mssql_mi/databases.tf
@@ -26,3 +26,32 @@ resource "null_resource" "retentiondays" {
     }
     }
 }
+
+resource "null_resource" "ltr_policy" {
+    depends_on          = [azurerm_mssql_managed_database.mssqlmi]
+    for_each    =        var.ltr_policy
+    triggers = {
+          weekly_retention  =  each.value.weekly_retention
+          monthly_retention =  each.value.monthly_retention
+          yearly_retention  =  each.value.yearly_retention
+          week_of_year      =  each.value.week_of_year
+    }
+    provisioner "local-exec" {
+         
+         command = <<-EOT
+           az sql midb ltr-policy set -g $resource_group --mi $servername -n $dbname --weekly-retention $weekly_retention --monthly-retention $monthly_retention --yearly-retention $yearly_retention --week-of-year $week_of_year
+         
+         EOT
+
+         environment = {
+          resource_group    =  var.resource_group_name
+          dbname            =  each.value.dbname
+          weekly_retention  =  self.triggers.weekly_retention
+          monthly_retention =  self.triggers.monthly_retention
+          yearly_retention  =  self.triggers.yearly_retention
+          week_of_year      =  self.triggers.week_of_year
+          servername        =  azurerm_mssql_managed_instance.mssqlmi.name
+         
+    }
+    }
+}
