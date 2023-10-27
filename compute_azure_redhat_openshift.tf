c72e3828660c15750f2da146707e97326907c1f0
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-10-27 16:56:39 +0300

    Update compute_azure_redhat_openshift.tf

--- compute_azure_redhat_openshift.tf
+++ compute_azure_redhat_openshift.tf
@@ -1,7 +1,7 @@
 module "aro" {
   source     = "./modules/compute/azure_redhat_openshift"
   for_each   = local.compute.aro_clusters
-  depends_on = [time_sleep.azurerm_role_assignment_for]
+  #depends_on = [time_sleep.azurerm_role_assignment_for]
 
   location                 = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_name      = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
