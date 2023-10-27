9c9928eb69a226a9a3f53642abe5da1764f342e0
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-10-27 16:06:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.4.0"
+      version = "~> 2.44.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
