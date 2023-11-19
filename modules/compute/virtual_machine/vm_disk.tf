26a0282edf558a00bafab59b3e02637051801022
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2023-11-19 16:03:35 +0200

    Update vm_disk.tf

--- modules/compute/virtual_machine/vm_disk.tf
+++ modules/compute/virtual_machine/vm_disk.tf
@@ -31,7 +31,7 @@ resource "azurerm_managed_disk" "disk" {
   disk_mbps_read_write       = try(each.value.disk.disk_mbps_read_write, null)
   tags                       = local.tags
   disk_encryption_set_id     = try(each.value.disk_encryption_set_key, null) == null ? null : var.disk_encryption_sets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.disk_encryption_set_key].id
-  source_resource_id         =  try(each.value.source_resource_id, null)
+  source_resource_id         = try(each.value.source_resource_id, null)
   hyper_v_generation         = try(each.value.hyper_v_generation, null)
   on_demand_bursting_enabled = try(each.value.on_demand_bursting_enabled, null)
 
