3c7ef044f765aad55d35171ff135e3b6dc164f74
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-05 11:38:36 +0200

    Update api_management.tf

--- api_management.tf
+++ api_management.tf
@@ -10,7 +10,7 @@ module "api_management" {
   vnets    = local.combined_objects_networking
 
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
-
+  public_ip_addresses = local.combined_objects_public_ip_addresses
   remote_objects = {
     resource_group     = local.combined_objects_resource_groups
     managed_identities = local.combined_objects_managed_identities
