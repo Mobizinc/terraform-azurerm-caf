f7f2f05dc153a2ecc06696fee086aed408426639
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-06 21:45:31 +0200

    Update diagnostic.tf

--- modules/webapps/appservice_slots/diagnostic.tf
+++ modules/webapps/appservice_slots/diagnostic.tf
@@ -2,7 +2,7 @@ module "diagnostics" {
   source = "../../diagnostics"
   count  = var.diagnostic_profiles == null ? 0 : 1
 
-  resource_id       = azurerm_app_service.app_service.id
+  resource_id       = azurerm_app_service_slot.slots.id
   resource_location = var.location
   diagnostics       = var.diagnostics
   profiles          = var.diagnostic_profiles
