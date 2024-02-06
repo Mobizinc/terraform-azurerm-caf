503ad2871f24973703eca8490af43a7da2c54c63
Thej Rayasam <trayasam@mobizinc.com>
2024-02-06 15:39:42 -0500

    Update module.tf

--- modules/logic_app/standard/module.tf
+++ modules/logic_app/standard/module.tf
@@ -70,7 +70,7 @@ resource "time_sleep" "wait_for_logic_app" {
   create_duration = "60s"
 }
 
-resource "null_resource" "logicapp_api_permission_dev1" {
+resource "null_resource" "logicapp_api_permission_dev" {
   depends_on = [time_sleep.wait_for_logic_app]
   count      = try(var.settings.name, null) == "iam-automation-nonprod-dev" ? 1 : 0     #var.name == "iam-automation-nonprod-dev" ? 1 : 0
 
@@ -80,7 +80,7 @@ resource "null_resource" "logicapp_api_permission_dev1" {
   }
 }
 
-resource "null_resource" "logicapp_api_permission_uat1" {
+resource "null_resource" "logicapp_api_permission_uat" {
   depends_on = [time_sleep.wait_for_logic_app]
   count      = try(var.settings.name, null) == "iam-automation-nonprod-uat" ? 1 : 0     #var.name == "iam-automation-nonprod-uat" ? 1 : 0
 
