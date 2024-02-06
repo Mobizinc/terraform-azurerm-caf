643eb61c9703b3c24a9581c83481c99b557b95b9
Thej Rayasam <trayasam@mobizinc.com>
2024-02-06 12:33:48 -0500

    Update variables.tf

--- modules/logic_app/standard/variables.tf
+++ modules/logic_app/standard/variables.tf
@@ -32,7 +32,8 @@ variable "vnets" {
   default = {}
 }
 variable "base_tags" {
-  default = {}
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
 }
 variable "identity" {
   default = null
