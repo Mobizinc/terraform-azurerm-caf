a9e678bafd051b193ca270096bbdc03a8efde511
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-08 19:54:08 +0200

    Update app_services.tf

--- app_services.tf
+++ app_services.tf
@@ -80,13 +80,14 @@ module "app_services_slots" {
   publish_profile      = try(each.value.publish_profile, null)
   vnet_integration     = try(each.value.vnet_integration, {}) 
   app_service_name     = each.value.app_service_name
+  app_services         = each.value.app_services
   private_dns          = local.combined_objects_private_dns  
   keyvault_name        = can(each.value.keyvault.key) ? local.combined_objects_keyvaults[try(each.value.keyvault.lz_key, local.client_config.landingzone_key)][each.value.keyvault.key].name : null
   remote_objects = {
     subnets            = try(local.combined_objects_networking[try(each.value.settings.lz_key, local.client_config.landingzone_key)][each.value.settings.vnet_key].subnets, null)
     vnets              = local.combined_objects_networking
     private_endpoints  = try(each.value.private_endpoints, {})
-    app_services       = local.combined_objects_app_services
+    
   }  
 }
 
