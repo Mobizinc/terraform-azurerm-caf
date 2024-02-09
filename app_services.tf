52909a4df9cb2d3b42e5b898ff82b6feec627e95
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-09 16:44:56 +0200

    Update app_services.tf

--- app_services.tf
+++ app_services.tf
@@ -71,7 +71,7 @@ module "app_services_slots" {
   global_settings      = local.global_settings
   combined_objects     = local.dynamic_app_settings_combined_objects
   base_tags            = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
-  application_insight  = try(each.value.application_insight_key, null) == null ? null : module.azurerm_application_insights[each.value.application_insight_key]
+  application_insight  = try(module.azurerm_application_insights[each.value.application_insight_key], null)
   diagnostic_profiles  = try(each.value.diagnostic_profiles, null)
   diagnostics          = local.combined_diagnostics
   storage_accounts     = local.combined_objects_storage_accounts
