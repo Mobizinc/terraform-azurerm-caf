30df690c1d020f0ac1ac8b19e8c4077b480405e7
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-09 14:32:44 +0200

    Create private_endpoints.tf

---
+++ modules/webapps/appservice_slot/private_endpoints.tf
@@ -0,0 +1,17 @@
+
+module "private_endpoint" {
+  source   = "../../networking/private_endpoint"
+  for_each = var.remote_objects.private_endpoints
+  depends_on = [azurerm_app_service_slot.slots]
+
+  resource_id         = var.app_services
+  location            = var.location
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  subnet_id           = try(var.remote_objects.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.remote_objects.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
+  settings            = each.value
+  global_settings     = var.global_settings
+  base_tags           = var.base_tags
+  private_dns         = var.private_dns
+  client_config       = var.client_config
+}
