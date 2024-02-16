a2a22058e62162542eb697f1b655f3bcd1db7db0
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-02-16 17:26:46 +0200

    Create module.tf

---
+++ modules/analytics/databricks_access_connector/module.tf
@@ -0,0 +1,17 @@
+resource "azurerm_databricks_access_connector" "databricks_access_connector" {
+  name                = "example-resource"
+  resource_group_name = var.resource_group
+  location            = var.location
+  tags                = local.tags
+
+  dynamic "identity" {
+    for_each = try(var.identity, null) == null ? [] : [1]
+
+    content {
+      type         = var.identity.type
+      identity_ids = lower(var.identity.type) == "userassigned" ? local.managed_identities : null
+    }
+  }
+  
+}
+
