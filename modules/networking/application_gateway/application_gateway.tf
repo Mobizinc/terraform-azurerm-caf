e4c3367137d7cfeed15d83cf4da5641dc986039f
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-04-03 12:31:06 +0300

    Update application_gateway.tf

--- modules/networking/application_gateway/application_gateway.tf
+++ modules/networking/application_gateway/application_gateway.tf
@@ -127,7 +127,7 @@ resource "azurerm_application_gateway" "agw" {
       host_name                      = try(trimsuffix((try(http_listener.value.host_names, null) == null ? try(var.dns_zones[try(http_listener.value.dns_zone.lz_key, var.client_config.landingzone_key)][http_listener.value.dns_zone.key].records[0][http_listener.value.dns_zone.record_type][http_listener.value.dns_zone.record_key].fqdn, http_listener.value.host_name) : null), "."), null)
       host_names                     = try(http_listener.value.host_name, null) == null ? try(http_listener.value.host_names, null) : null
       require_sni                    = try(http_listener.value.require_sni, false)
-      ssl_certificate_name           = try(try(try(http_listener.value.keyvault_certificate_request.key, http_listener.value.keyvault_certificate.certificate_key), data.azurerm_key_vault_certificate.manual_certs[http_listener.key].name), null)
+      ssl_certificate_name           = try(try(try(http_listener.value.keyvault_certificate_request.ssl_certificate_name, http_listener.value.keyvault_certificate.certificate_key), data.azurerm_key_vault_certificate.manual_certs[http_listener.key].name), null)
       firewall_policy_id             = try(var.application_gateway_waf_policies[try(http_listener.value.waf_policy.lz_key, var.client_config.landingzone_key)][http_listener.value.waf_policy.key].id, null)
     }
   }
